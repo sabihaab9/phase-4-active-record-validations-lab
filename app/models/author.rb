9a741d86bd54b8a0c1d0c57053a2a502a125a15c
Sabiha Bary <sabihaab9@gmail.com>
2022-08-08 16:43:39 -0400

    commit tree a39169587293be344e862d7ae2b8b4c0f0ee2370

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ApplicationRecord
+
+    #all authors have a name, no two authors have the same exact name 
+    validates :name, presence: true, uniqueness: true
+
+    validates :phone_number, length: {is: 10} 
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ApplicationRecord
+
+#validates isn a helper method call 
+
+    #validates :attribute_name, helper_name: {hash-of-options}
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    #call the #validate method
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+      ]
+
+      def clickbait?
+        if      CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+      end
+    
 end
+
